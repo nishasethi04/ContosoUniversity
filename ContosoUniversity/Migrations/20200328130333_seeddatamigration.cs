36d3c37550620d0c7315f504ba5e6316167f5094
nishasethi04 <nisethi85@gmail.com>
2020-03-28 12:54:41 -0400

    first commit

---
+++ ContosoUniversity/Migrations/20200328130333_seeddatamigration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class seeddatamigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Course",
+                columns: new[] { "CourseID", "Credits", "Title" },
+                values: new object[] { 1050, 3, "Chemistry" });
+
+            migrationBuilder.InsertData(
+                table: "Student",
+                columns: new[] { "ID", "EnrollmentDate", "FirstMidName", "LastName" },
+                values: new object[] { 1, new DateTime(2005, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Carson", "Alexander" });
+
+            migrationBuilder.InsertData(
+                table: "Enrollment",
+                columns: new[] { "EnrollmentID", "CourseID", "Grade", "StudentID" },
+                values: new object[] { 101, 1050, 0, 1 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Enrollment",
+                keyColumn: "EnrollmentID",
+                keyValue: 101);
+
+            migrationBuilder.DeleteData(
+                table: "Course",
+                keyColumn: "CourseID",
+                keyValue: 1050);
+
+            migrationBuilder.DeleteData(
+                table: "Student",
+                keyColumn: "ID",
+                keyValue: 1);
+        }
+    }
+}
